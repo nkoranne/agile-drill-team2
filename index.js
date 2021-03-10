1c6baa2c4138b74589f086e1d733c0968603c24d
James Wright <jamesw@kainos.com>
2021-03-10 13:12:07 +0000

    started form

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const app = express()
 const port = 3000
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Now listening at http://localhost:${port}`)
 })
 
 app.get('/', (req,res) => {
     res.sendFile('index.html', {root: __dirname})
+})
+
+app.get('/addCourse', (req,res) => {
+  res.sendFile('addNewCourse.html', {root: __dirname})
 })
