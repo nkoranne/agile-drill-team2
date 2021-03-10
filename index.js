73bd566dfa09a68d1dcacb8579e41aa842ffb737
catherineed <56081396+catherineed@users.noreply.github.com>
2021-03-10 15:21:35 +0000

    updating loading page

--- index.js
+++ index.js
@@ -6,21 +6,22 @@ app.listen(port, () => {
   console.log(`Now listening at http://localhost:${port}`)
 })
 
-app.get('/', (req,res) => {
-    res.sendFile('index.html', {root: __dirname})
+app.get('/', (req, res) => {
+  res.sendFile('courses.html', { root: __dirname })
 })
 
-app.get('/addCourse', (req,res) => {
-    res.sendFile('addNewCourse.html', {root: __dirname})
+app.get('/addCourse', (req, res) => {
+  res.sendFile('addNewCourse.html', { root: __dirname })
 })
 
-app.get('/addEvent', (req,res) => {
-  res.sendFile('addNewEvent.html', {root: __dirname})
+app.get('/addEvent', (req, res) => {
+  res.sendFile('addNewEvent.html', { root: __dirname })
 })
 
 app.get('/courses', (req, res) => {
-    res.sendFile('courses.html', {
-    root: __dirname})
+  res.sendFile('courses.html', {
+    root: __dirname
+  })
 })
 
 app.get('/course-1', (req, res) => {
