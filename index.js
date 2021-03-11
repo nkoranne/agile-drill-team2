e174f5e5e47dcc4ee4b83b669c5a11fc923f1b86
James Wright <jamesw@kainos.com>
2021-03-11 15:32:30 +0000

    add course working

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(bodyParser.urlencoded({
 
 var connection = mysql.createConnection({
   socketPath: '/tmp/mysql.sock',
+  host: 'localhost',
   user: 'root',
   password: 'password',
   database: 'MapDatabase'
@@ -22,19 +23,19 @@ connection.connect(function (err) {
 });
 
 app.listen(port, () => {
-  console.log(`Now listening at http://localhost:${port}`)
+  console.log(`Now listening at http://localhost:${port}`);
 })
 
 app.get('/', (req, res) => {
-  res.render('courses', {})
+  res.render('courses', {});
 })
 
 app.get('/addCourse', (req, res) => {
-  res.render('addNewCourse', {})
+  res.render('addNewCourse', {});
 })
 
 app.get('/addEvent', (req, res) => {
-  res.render('addNewEvent', {})
+  res.render('addNewEvent', {});
 })
 
 app.get('/courses', (req, res) => {
@@ -78,19 +79,61 @@ e.g. req.body.courseName if courseName was the name of the element/name tag in H
 */
 
 app.post('/add-course', (req, res) => {
+  console.log("adding course");
   //READ INPUT FROM FORM
+  var title = req.body.title;
+  var description = req.body.description;
+  var duration = req.body.duration;
+  var preferred = req.body.preferred;
+
+  //Validation
+
+  //Title is less than 100 characters
+  if (title.length > 100) {
+    res.render('addNewCourse', {});
+  }
+
+  //Description is less than 300 characters
+  if (description.length > 300) {
+    res.render('addNewCourse', {});
+  }
+
+  //Preferred Attendee list is less than 200 characters
+  if (preferred.length > 200) {
+    res.render('addNewCourse', {});
+  }
 
   //USE INPUT TO MAKE AN INSERT STATEMENT INTO DB
-  connection.query('QUERY HERE', function (error, results, fields) {
+  connection.query('INSERT INTO Course (course_title, course_description, course_duration, PreferedAttendees) VALUES (\'' + title + '\', \'' + description + '\', \'' + duration + '\', \'' + preferred + '\');', function (error, results, fields) {
     if (error) throw error;
   });
+
+  res.render('addNewCourse', {});
 })
 
 app.post('/add-event', (req, res) => {
+  console.log("adding event ", req.body);
    //READ INPUT FROM FORM
 
+   var course_id = req.body.course_id;
+   var title = req.body.title;
+   var date = req.body.date;
+   var locationDropdown = req.body.locationDropdown;
+
+   console.log(course_id);
+   console.log(title);
+
+   /*if (title == null) {
+    connection.query('SELECT * FROM Course where course_id = ' + course_id, function (error, results, fields) {
+      if (error) throw error;
+        console.log(results)
+    });
+   }*/
+
    //USE INPUT TO MAKE AN INSERT STATEMENT INTO DB
-  connection.query('QUERY HERE', function (error, results, fields) {
+   /*connection.query('INSERT INTO Events (event_title, event_location, event_date, trainer_id, course_id) VALUES (\'' + title + '\', \'' + description + '\', \'' + duration + '\', \'' + preferred + '\');', function (error, results, fields) {
     if (error) throw error;
-  });
+  });*/
+
+  //res.render('addNewEvent', {})
 })
